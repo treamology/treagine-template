fe3c371cda3dbdb8403c9a6bb40cc097120a6d76
Donny Lawrence <trea@treamous.com>
2016-06-13 10:42:52 -0500

    Add the engine as a submodule, make a change to prevent crashing.

--- game.lua
+++ game.lua
@@ -3,13 +3,13 @@ local GameManager = require "treagine.game.gamemanager"
 local Game = GameManager:extend("My Game") -- your game's name
 
 function Game:load()
-	Game.super:load()
+	Game.super.load(self)
 
 	love.window.setTitle(self.name)
 end
 
 function Game:update(dt)
-	Game.super:update()
+	Game.super.update(self, dt)
 end
 
 return Game
